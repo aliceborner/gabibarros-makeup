c80c90aed6cfd1807de27a9f9ac50251e2477913
Alice Borner <alicezbo@gmail.com>
2020-11-22 17:57:12 +0100

    Adding pages components

---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import Cards from "../Cards";
+import MainSection from "../MainSection.js";
+
+function Home() {
+  return (
+    <>
+      <MainSection />
+      <Cards />
+    </>
+  );
+}
+
+export default Home;
