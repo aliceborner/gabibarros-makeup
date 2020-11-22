0aed76611bc9fa1ab9f7abe615df27d26f68b663
Alice Borner <alicezbo@gmail.com>
2020-11-22 17:58:14 +0100

    Adding Route to Home on App js and background image on css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home.js";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar></Navbar>
-      <Switch>
-        <Route path='/' exact />
-      </Switch>
-    </Router>
+      <Router>
+        <Navbar></Navbar>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
     </>
   );
 }
