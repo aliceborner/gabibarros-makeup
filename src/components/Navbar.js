770d3349f3609ef13073867bcd3acd9e4a85ef1f
Alice Borner <alicezbo@gmail.com>
2020-12-06 15:44:50 +0100

    Adding Navbar container back

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "./Button";
 import "./Navbar.css";
 
 function Navbar() {
@@ -25,43 +24,44 @@ function Navbar() {
   window.addEventListener("resize", showButton);
 
   return (
-    <nav className="navbar">
-      <Link to="/" className="navbar-logo">
-        Gabriela Barros <span class="icon"></span>
-      </Link>
-
-      <div className="menu-icon" onClick={handleClick}>
-        <i className={click ? "far fa-times" : "fas fa-bars"} />
-      </div>
-      <ul className={click ? "nav-menu active" : "nav-menu"}>
-        <li className="nav-item">
-          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-            Home
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Gabriela Barros <span class="icon"></span>
           </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/blog" className="nav-links" onClick={closeMobileMenu}>
-            Blog
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/courses" className="nav-links" onClick={closeMobileMenu}>
-            Courses
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
-            About me
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-      {button && <Button buttonStyle="btn--outline">Test</Button>}
-    </nav>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/blog" className="nav-links" onClick={closeMobileMenu}>
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                Sobre mim
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contato
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
