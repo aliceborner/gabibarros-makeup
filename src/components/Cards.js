1896f8ee389831e7b6518edf2f6931db77e4d130
Alice Borner <alicezbo@gmail.com>
2020-12-06 15:43:20 +0100

    Renaming cards and adjusting overflow

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,48 +1,48 @@
 import React from "react";
+import "./Cards.css";
 import CardItem from "./CardItem";
-import "../components/Cards.css";
 
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these epic makeups!</h1>
+      <h1>Belezas por mim</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
-          <ul className="cards__item">
+          <ul className="cards__items">
             <CardItem
               src="/images/img-1.jpg"
-              text="Test"
+              text="Peles negras e seus detalhes"
               label="black"
               path="/contact"
             ></CardItem>
             <CardItem
               src="/images/img-2.jpg"
-              text="Test"
-              label="self"
+              text="Criatividade à flor da pele"
+              label="creative"
               path="/contact"
             ></CardItem>
             <CardItem
               src="/images/img-3.jpg"
-              text="Test"
+              text="Pedrinhas porque sim!"
               label="blue"
               path="/contact"
             ></CardItem>
             <CardItem
               src="/images/img-4.jpg"
-              text="Test"
+              text="Diferente é ser você e ser você é ser único"
               label="skin"
               path="/contact"
             ></CardItem>
             <CardItem
               src="/images/img-5.jpg"
-              text="Test"
+              text="Rosinha porque é minha paixão"
               label="pink"
               path="/contact"
             ></CardItem>
             <CardItem
               src="/images/img-6.jpg"
-              text="Test"
-              label="gold"
+              text="La vie en rosé, em Fracês pra fingir que sou chique"
+              label="rosé"
               path="/contact"
             ></CardItem>
           </ul>
